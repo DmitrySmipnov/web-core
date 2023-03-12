8c366a7ff19ac4dd93c911b547944e32036a0bac
Dmitry Smirnov <smipnovdv@gmail.com>
2023-03-12 12:24:54 +0300

    доделал свайпер

--- index.js
+++ index.js
@@ -4,6 +4,8 @@ if (sizeWiewport.matches){
     new Swiper('.swiper',{
         width: 240,
 
+        // spaceBetween: 16,
+
         pagination: {
             el: '.swiper-pagination',
             type: 'bullets',
